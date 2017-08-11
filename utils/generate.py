f62f1fa68bb9fceb9b00c9dc75caa128a93ae3a6
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-08-11 18:46:12 +0200

    Fixed some attribute ids

--- utils/generate.py
+++ utils/generate.py
@@ -94,6 +94,35 @@ def get_resources_by_provider(provider):
     uls = soup.find_all('ul', class_='nav-visible')
     return [(a.get_text(), a['href']) for ul in uls for a in ul.find_all('a')]
 
+def arghelper(kind, soup):
+    arglist = []
+    argumentrefs = soup.find_all('h2', id='%s-reference' % kind)
+    if len(argumentrefs) > 0:
+        argumentref = argumentrefs[0]
+        ul = get_sibling(argumentref)
+        while ul is not None:
+            if ul.name == 'ul':
+                lis = ul.find_all('li', recursive=False)
+                for li in lis:
+                    codes = li.find_all('code')
+                    if len(codes) > 0:
+                        argname = codes[0].get_text()
+                    else:
+                        aas = li.find_all('a')
+                        if len(aas) is 0:
+                            continue
+                        if aas[0].get('name') is not None:
+                            argname = aas[0]['name']
+                        else:
+                            argname = li.get_text().split('-')[0].strip()
+                    argdoc = li.get_text().replace("\n", " ").strip()
+                    arglist.append({'name': argname, 'doc': argdoc.translate(escaping)})
+                break
+            if ul.name == 'h2':
+                break
+            ul = get_sibling(ul)
+    return arglist
+
 def get_resource_params(name, docpath):
     page = s.get("https://www.terraform.io%s" % docpath)
     kind = {'r': 'resource', 'd': 'data', 'external': 'data', 'http': 'data'}[docpath.split('/')[-2]]
@@ -110,53 +139,9 @@ def get_resource_params(name, docpath):
         maindoc += p.get_text().replace("\n", " ") + "\n\n"
         p = get_sibling(p)
     maindoc = maindoc.strip()
-    
-    arglist = []
-    argumentrefs = soup.find_all('h2', id='argument-reference')
-    if len(argumentrefs) > 0:
-        argumentref = argumentrefs[0]
-        ul = get_sibling(argumentref)
-        while ul is not None:
-            if ul.name == 'ul':
-                lis = ul.find_all('li', recursive=False)
-                for li in lis:
-                    aas = li.find_all('a')
-                    if len(aas) is 0:
-                        continue
-                    if aas[0].get('name') is not None:
-                        argname = aas[0]['name']
-                    else:
-                        argname = li.get_text().split('-')[0].strip()
-                    argdoc = li.get_text().replace("\n", " ").strip()
-                    arglist.append({'name': argname, 'doc': argdoc.translate(escaping)})
-                break
-            if ul.name == 'h2':
-                break
-            ul = get_sibling(ul)
-    
-    attrlist = []
-    attributerefs = soup.find_all('h2', id='attributes-reference')
-    if len(attributerefs) > 0:
-        attributeref = attributerefs[0]
-        ul = get_sibling(attributeref)
-        while ul is not None:
-            if ul.name == 'ul':
-                lis = ul.find_all('li', recursive=False)
-                for li in lis:
-                    aas = li.find_all('a')
-                    if len(aas) is 0:
-                        continue
-                    if aas[0].get('name') is not None:
-                        attrname = aas[0]['name']
-                    else:
-                        attrname = li.get_text().split('-')[0].strip()
-                    assert(attrname != "Data Source")
-                    attrdoc = li.get_text().replace("\n", " ").strip()
-                    attrlist.append({'name': attrname, 'doc': attrdoc.translate(escaping)})
-                break
-            if ul.name == 'h2':
-                break
-            ul = get_sibling(ul)
+
+    arglist  = arghelper('argument', soup)
+    attrlist = arghelper('attributes', soup)
 
     return {
         'name': name,
@@ -190,15 +175,10 @@ def gather_all_by_provider(provider):
     global resource_list
     print("Gathering resources for %s" % provider)
     resources = get_resources_by_provider(provider)
-    i = 0
     for name, docpath in resources:
         print(name)
         params = get_resource_params(name, docpath)
-        #print(json.dumps(params,indent=2,sort_keys=True))
         resource_list.append(params)
-        #if i > 10:
-        #    break
-        i += 1
 
 def prepare_file():
     print("Generating file...")
@@ -281,7 +261,7 @@ def prepare_file():
         file.write(header + data + footer)
     
 get_providers()
-#for p in ['rundeck']:
+#for p in ['alicloud']:
 for p in provider_list:
     gather_all_by_provider(p)
 get_interpolation_functions()
